1088054882109b936acc650ca9ec9b656752baef
futurebound <mitchellm1618@gmail.com>
2024-02-09 22:27:28 -0800

    added app/_layout.js and app/index.js

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+// create a layour route in app/_layout.js to render a native stack navigator
+
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+   return <Stack />;
+}
+
+export default Layout;
